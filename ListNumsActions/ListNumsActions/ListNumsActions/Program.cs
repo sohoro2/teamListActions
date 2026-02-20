fa89caa865aaee51f9a8d0274de21bbf4e9bd80d
Mitko <d.dimitrov2022@pmg-pernik.com>
2026-02-20 10:13:58 +0200

    Project

---
+++ ListNumsActions/ListNumsActions/ListNumsActions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListNumsActions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string[] cmd = Console.ReadLine().Split().ToArray();
+                string command = cmd[0];
+                if (command.ToLower() == "finish")
+                {
+                    break;
+                }
+                switch (command)
+                {
+                    case "ins":
+                        //TODO
+                        break;
+                    case "del":
+                        //TODO
+                        break;
+                    case "contains":
+                        //TODO
+                        break;
+                    case "remove":
+                        //TODO
+                        break;
+                    //TODO
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
